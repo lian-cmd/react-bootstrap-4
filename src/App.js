a086ceb7f0dd3c579947bc826f9ee7b780464645
lian-cmd <lianabdiansyaht@gmail.com>
2021-08-31 14:41:43 +0700

    Menambahkan Komponen About dan HomePage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Button } from 'reactstrap';
 import NavbarComp from './Components/Fungsi/NavbarComp';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import HomePage from './Components/Fungsi/HomePage';
+import About from './Components/Fungsi/About';
 
 function App() {
   return (
-    <div>
-      <NavbarComp/>
-    </div>
+    <BrowserRouter>
+         <NavbarComp/>
+         <Switch>
+           <Route exact path="/" component={HomePage}></Route>
+           <Route exact path="/about" component={About}></Route>
+         </Switch>
+    </BrowserRouter>
+    
   );
 }
 
